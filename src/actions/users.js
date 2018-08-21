f330dec5823c0e8aabbf60a6f7996ef3514499a0
Michael Le-Reiver <m.lereiver@gmail.com>
2018-08-21 13:36:33 -0700

    modified registration to accept phonenumber

--- src/actions/users.js
+++ src/actions/users.js
@@ -4,6 +4,7 @@ import {API_BASE_URL} from '../config';
 import {normalizeResponseErrors} from './utils';
 
 export const registerUser = user => dispatch => {
+    console.log(user);
     return fetch(`${API_BASE_URL}/users`, {
         method: 'POST',
         headers: {
