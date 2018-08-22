7045f00562b004e69a15cf5ae93e1e6db926fe66
Suman Bandita <sumanbandita@gmail.com>
2018-08-21 21:09:03 -0400

    tabs

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import requiresLogin from './requires-login';
 import {fetchProtectedData} from '../actions/protected-data';
+import Tab from './tab';
 
 export class Dashboard extends React.Component {
     componentDidMount() {
@@ -12,12 +13,14 @@ export class Dashboard extends React.Component {
         return (
             <div className="dashboard">
                 <div className="dashboard-username">
-                    Username: {this.props.username}
+                    {this.props.username}
                 </div>
-                <div className="dashboard-name">Name: {this.props.name}</div>
+                <Tab />
+                {/* <div className="dashboard-name">Name: {this.props.name}</div>
                 <div className="dashboard-protected-data">
                     Protected data: {this.props.protectedData}
-                </div>
+                </div> */}
+
             </div>
         );
     }
