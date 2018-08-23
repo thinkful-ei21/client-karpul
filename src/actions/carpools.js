32eb2bada7dcadda53cf65a4585bd50a358b45c3
Joseph Kim <gyuhankim@gmail.com>
2018-08-23 11:15:10 -0400

    pseuocode for actions/reducer

--- src/actions/carpools.js
+++ src/actions/carpools.js
@@ -3,21 +3,35 @@ import {SubmissionError} from 'redux-form';
 import {API_BASE_URL} from '../config';
 import {normalizeResponseErrors} from './utils';
 
-// FETCH_CARPOOLS_REQUEST
-// FETCH_CARPOOLS_SUCCESS
-// FETCH_CARPOOLS_ERROR
-// fetchCarpools
+/* For My Carpools Page */
+// FETCH_USER_CARPOOLS_REQUEST
+// FETCH_USER_CARPOOLS_SUCCESS
+// FETCH_USER_CARPOOLS_ERROR
+// fetchUserCarpools
 
+/* For expanded view */
 // FETCH_ONE_CARPOOL_REQUEST
 // FETCH_ONE_CARPOOL_SUCCESS
 // FETCH_ONE_CARPOOL_ERROR
 // fetchOneCarpool
 
-// HOST ONLY ACTIONS //
+/* For Find Carpools Page */
+// CARPOOL_PROXIMITY_SEARCH_REQUEST
+// CARPOOL_PROXIMITY_SEARCH_SUCCESS
+// CARPOOL_PROXIMITY_SEARCH_ERROR
+// fetchNearbyCarpools
+
+/* Editable Carpools Fields by HOST */
 // UPDATE_CARPOOL_REQUEST
 // UPDATE_CARPOOL_SUCCESS
 // UPDATE_CARPOOL_ERROR
 // updateCarpool
+
+/* User clicking "Request To Join" button */
+// REQUEST_CARPOOL_INVITE_REQUEST
+// REQUEST_CARPOOL_INVITE_SUCCESS
+// REQUEST_CARPOOL_INVITE_ERROR
+// requestCarpoolInvite
 
 export const createNewCarpool = carpool => (dispatch, getState) => {
     const authToken = getState().auth.authToken;
