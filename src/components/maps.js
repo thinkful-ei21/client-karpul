3a235e44126b0f721f41f7a126565b0d3e2d2134
Michael Le-Reiver <m.lereiver@gmail.com>
2018-08-29 12:57:17 -0700

    code cleanup on map.js

--- src/components/maps.js
+++ src/components/maps.js
@@ -15,15 +15,13 @@ export class Maps extends Component {
         height: 600,
         latitude: 45.52165,
         longitude: -122.67685,
-        // latitude: 0,
-        // longitude: 0,
         zoom: 12
       }
     }
+
     const width = window.innerWidth;
 
     if (width < 800) {
-      console.log(window.innerWidth)
       this.state = {
         viewport: {
           width: 600,
@@ -36,7 +34,6 @@ export class Maps extends Component {
     }
 
     if (width < 600) {
-      console.log(window.innerWidth)
       this.state = {
         viewport: {
           width: 300,
@@ -52,121 +49,6 @@ export class Maps extends Component {
 
 componentDidMount(){
   // console.log(this.props.coordinates[0].longitude);
-  // window.addEventListener("resize", function() { 
-    console.log(window.innerWidth)
-    // const width = window.innerWidth;
-
-    // if (width <= 800) {
-    //   console.log('resizing map')
-    //   this.setState({
-    //     viewport: {
-    //     width: 400,
-    //     height: 300
-    //     }
-    //   })
-    // }
-
-      // else {
-      //   this.state = {
-      //     viewport: {
-      //       width: 1024,
-      //       height: 400,
-      //       latitude: 45.52165,
-      //       longitude: -122.67685,
-      //       // latitude: 0,
-      //       // longitude: 0,
-      //       zoom: 8
-      //   }
-      // }
-    // }
-  // });
-
-
-  //   // have min value, set ratio take % of screen set viewport to corresponding % 
-  //   // if window.innerHeight ...
-  //   switch(window.innerWidth) {
-  //     // case ( this > 1280):
-  //     //   return this.setState({
-  //     //     viewport: {
-  //     //     width: 1200,
-  //     //     height: 500
-  //     //     }
-  //     //   })
-  //     //   break;
-
-  //     case ( this > 1280):
-  //     this.setState(([...state], props) => {
-  //       return {
-  //         viewport: {
-  //         width: 1200,
-  //         height: 500
-  //         }
-  //       }
-  //     })
-  //     break;
-
-  //     // case ( this > 768):
-  //     // this.setState((...state, props) => {
-  //     //     viewport: {
-  //     //     width: 700,
-  //     //     height: 300
-  //     //     }
-        
-  //     //   })
-  //     //   break;
-
-  //     case ( this > 768):
-  //     this.setState(([...state], props) => {
-  //       return {
-  //         viewport: {
-  //         width: 700,
-  //         height: 300
-  //         }
-  //       }
-  //     })
-  //     break;
-
-  //     // case ( this > 640):
-  //     // return this.setState({
-  //     //     viewport: {
-  //     //     width: 600,
-  //     //     height: 300
-  //     //     }
-  //     //   })
-  //     //   break;
-
-  //     case ( this > 640):
-  //     this.setState(([...state], props) => {
-  //       return {
-  //         viewport: {
-  //         width: 600,
-  //         height: 300
-  //         }
-  //       }
-  //     })
-  //     break;
-
-  //     case ( this > 400):
-  //     return this.setState({
-  //         viewport: {
-  //         width: 350,
-  //         height: 300
-  //         }
-  //       })
-  //       break;
-  //     case ( this > 320):
-  //     this.setState(([...state], props) => {
-  //       return {
-  //           viewport: {
-  //           width: 300,
-  //           height: 200
-  //           }
-  //         }
-  //       })
-  //       break;
-  //   }
-  //   return this.state;
-  // });
 }
 
 handleClick(e) {
